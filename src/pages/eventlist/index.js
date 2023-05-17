b937b5d9f5d4a4e064ca715a75014eab5e249429
d-dolin96 <d.dolinski1996@.com>
2023-05-16 22:05:14 -0400

    Text input and Event list page added

---
+++ src/pages/eventlist/index.js
@@ -0,0 +1,9 @@
+function Eventlist() {
+  return (
+    <>
+      <h1>This is the Event Page</h1>
+    </>
+  );
+}
+
+export default Eventlist;
--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,12 +1,15 @@
 import Logo from "../../design/atoms/logo";
 import Button from "../../design/atoms/button";
 import Avatar from "../../design/atoms/avatar";
+import Textbox from "../../design/atoms/textbox";
 
 function Settings() {
   return (
     <div>
       <Logo></Logo>
       <Avatar></Avatar>
+      <Textbox></Textbox>
+      <Textbox></Textbox>
       <Button></Button>
     </div>
   );
