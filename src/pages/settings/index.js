79b0f259bb81c6fc3fef0babb79860d4f7e789de
ANTIM3CH <austinloganart@gmail.com>
2023-05-16 22:19:43 -0400

    Added Avatar

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,12 +1,14 @@
 import Logo from "../../design/atoms/logo";
 import Button from "../../design/atoms/button";
 import Avatar from "../../design/atoms/avatar";
+import Checkbox from "../../design/atoms/checkbox";
 
 function Settings() {
   return (
     <div>
       <Logo></Logo>
       <Avatar></Avatar>
+      <Checkbox></Checkbox>
       <Button></Button>
     </div>
   );
