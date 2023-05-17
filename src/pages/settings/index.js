c9f32a56285d653f52845a05a41f13dc35815666
GuiltyDogg <drewschultz1993@gmail.com>
2023-05-16 22:05:29 -0400

    Icon index

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,12 +1,14 @@
 import Logo from "../../design/atoms/logo";
 import Button from "../../design/atoms/button";
 import Avatar from "../../design/atoms/avatar";
+import Icon from "../../design/atoms/icon";
 
 function Settings() {
   return (
     <div>
       <Logo></Logo>
       <Avatar></Avatar>
+      <Icon></Icon>
       <Button></Button>
     </div>
   );
