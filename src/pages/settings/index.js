431b12dacb031ff88f1ba68a1cd8d67663651f4b
GuiltyDogg <drewschultz1993@gmail.com>
2023-05-16 22:42:30 -0400

    Fix settings?

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,12 +1,20 @@
 import Logo from "../../design/atoms/logo";
 import Button from "../../design/atoms/button";
 import Avatar from "../../design/atoms/avatar";
+import Icon from "../../design/atom/icon";
+import Checkbox from "../../design/atoms/checkbox";
+import Textbox from "../../design/atoms/textbox";
+import Textinput from "../../design/atoms/textinput";
 
 function Settings() {
   return (
     <div>
       <Logo></Logo>
       <Avatar></Avatar>
+      <Textinput></Textinput>
+      <Textbox></Textbox>
+      <Icon></Icon>
+      <Checkbox></Checkbox>
       <Button></Button>
     </div>
   );
