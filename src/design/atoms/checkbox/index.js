25fec88df8b7944c66b3b6b81f628e09136d4a9a
Todd Little <toddlittle@Todds-MacBook-Pro.local>
2023-05-23 23:00:48 -0400

    add state to checkbox

--- src/design/atoms/checkbox/index.js
+++ src/design/atoms/checkbox/index.js
@@ -1,14 +1,27 @@
+import React, { useState, useCallback} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
 import { faSquare } from "@fortawesome/free-regular-svg-icons";
 import { StyledCheck } from "./styles";
 
 function Checkbox() {
+  const [checked, setChecked] = useState(false);
+
+  const clickHandler = useCallback((e) => {
+    setChecked(!checked);
+  }, [checked]);
+
   return (
-    <StyledCheck>
-      Allow AngelChoir to use location <FontAwesomeIcon icon={faSquare} />
+    <StyledCheck onClick={clickHandler}>
+      Allow AngelChoir to use location? { checked ? (
+        <FontAwesomeIcon icon={faSquareCheck} />
+      ) : (
+        <FontAwesomeIcon icon={faSquare} />
+      )}
     </StyledCheck>
   );
 }
 
 export default Checkbox;
+
+
--- src/design/atoms/checkbox/styles.js
+++ src/design/atoms/checkbox/styles.js
@@ -4,4 +4,9 @@ import whitespace from "../../subatomics/whitespace";
 export const StyledCheck = styled.label`
   margin: ${whitespace.base} 0;
   display: block;
+
+  svg {
+    margin-left: ${ whitespace.less };
+  }
 `;
+
