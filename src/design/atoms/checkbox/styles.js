b186e10959014399a62ec0380b87c7fdd0346174
d-dolin96 <d.dolinski1996@.com>
2023-05-23 22:32:29 -0400

    Settings page styles

---
+++ src/design/atoms/checkbox/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import whitespace from "../../subatomics/whitespace";
+
+export const StyledCheck = styled.label`
+  margin: ${whitespace.base} 0;
+  display: block;
+`;
