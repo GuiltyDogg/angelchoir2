c3361b5fabe227a09b2f0d5c6b60242def800b42
GuiltyDogg <drewschultz1993@gmail.com>
2023-05-29 13:13:52 -0400

    PROFILE PIC UPLOAD BABY (needs frame)

--- src/design/atoms/avatar/index.js
+++ src/design/atoms/avatar/index.js
@@ -1,39 +1,51 @@
+import { useState } from "react";
 import { AvatarStyles } from "./styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 function Avatar() {
-  return (
-    // <AvatarStyles
-    //   src="./images/8324223_ui_essential_app_avatar_profile_icon.png"
-    //   alt="No Avatar"
-    // />
+  const [selectedImage, setSelectedImage] = useState(null);
 
-    <AvatarStyles>
-      <FontAwesomeIcon icon={faUser} />
-    </AvatarStyles>
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const imageUrl = URL.createObjectURL(file);
+    setSelectedImage(imageUrl);
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept="image/*"
+        id="avatar-upload"
+        onChange={handleImageUpload}
+        style={{ display: "none" }}
+      />
+      <label htmlFor="avatar-upload">
+        <AvatarStyles>
+          {selectedImage ? (
+            <img src={selectedImage} alt="User Avatar" />
+          ) : (
+            <FontAwesomeIcon icon={faUser} />
+          )}
+        </AvatarStyles>
+      </label>
+    </div>
   );
 }
 
 export default Avatar;
 
-// import { useState } from "react";
 // import { AvatarStyles } from "./styles";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 // function Avatar() {
-//   const [selectedImage, setSelectedImage] = useState(null);
-
-//   const handleImageUpload = (event) => {
-//     const file = event.target.files[0];
-//     const imageUrl = URL.createObjectURL(file);
-//     setSelectedImage(imageUrl);
-//   };
-
 //   return (
-//     <div>
-//       <input type="file" accept="image/*" onChange={handleImageUpload} />
-//       <AvatarStyles src={selectedImage} alt="No Avatar" />
-//     </div>
+
+//     <AvatarStyles>
+//       <FontAwesomeIcon icon={faUser} />
+//     </AvatarStyles>
 //   );
 // }
 
