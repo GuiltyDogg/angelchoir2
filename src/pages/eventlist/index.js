5acd58d36b0054c9ff11e6f0022ecb19c0086738
ANTIM3CH <austinloganart@gmail.com>
2023-06-07 21:26:04 -0400

    Added EventPage

--- src/pages/eventlist/index.js
+++ src/pages/eventlist/index.js
@@ -1,9 +1,11 @@
 import NavButton from "../../design/atoms/navbutton";
+import Events from "../../design/organisms/eventspage";
 
 function Eventlist() {
   return (
     <>
       <NavButton></NavButton>
+      <Events></Events>
     </>
   );
 }
