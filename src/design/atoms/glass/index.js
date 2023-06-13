203b27f38c3a6493d62d7a962a2d456130cd292f
GuiltyDogg <drewschultz1993@gmail.com>
2023-06-13 16:24:27 -0400

    glass card started

--- src/design/atoms/glass/index.js
+++ src/design/atoms/glass/index.js
@@ -1,9 +1,15 @@
 import { OverlayPage, Window } from "./styles";
+import { EventTitle } from "../../organisms/eventspage/styles";
+import { ButtonStyles } from "../button/styles";
 
 function GlassWindow() {
   return (
     <Window>
-      <h1>balls</h1>
+      <EventTitle>CHECK IN</EventTitle>
+      <br></br>
+      EVENT INFO HERE
+      <br></br>
+      <ButtonStyles>YEAH CHECK ME IN</ButtonStyles>
     </Window>
   );
 }
--- src/design/atoms/glass/styles.js
+++ src/design/atoms/glass/styles.js
@@ -1,10 +1,27 @@
 import styled from "styled-components";
+import whitespace from "../../subatomics/whitespace";
+import fonts from "../../subatomics/fonts";
+import borderradius from "../../subatomics/borderradius";
+
+// export const OverlayPage = styled.div`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   width: 100vw;
+//   height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
 
 export const Window = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: white;
+  padding: ${whitespace.base};
+  background-color: rgba(255, 255, 255, 0.5);
   display: flex;
-  justify-content: center;
-  opacity: 70%;
+  flex: 0 1 auto;
+  flex-direction: column;
+  height: 50%;
+  border-radius: ${borderradius.most};
+  text-align: center;
+  font-family: ${fonts.font};
 `;
