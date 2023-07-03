13fac282649a0fa0e0c48023fb4ce1a15232c00c
d-dolin96 <d.dolinski1996@.com>
2023-07-03 15:17:32 -0400

    slight tweak avatar

--- src/design/atoms/avatar/styles.js
+++ src/design/atoms/avatar/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import whitespace from "../../subatomics/whitespace";
 import borderradius from "../../subatomics/borderradius";
+import colors from "../../subatomics/colors";
 
 export const AvatarCircle = styled.div`
   display: flex;
@@ -10,8 +10,8 @@ export const AvatarCircle = styled.div`
   height: 25vh;
   max-height: 25vh;
   border-radius: ${borderradius.circle};
-  background: #bf87f6;
-  box-shadow: inset 21px -21px 42px #b580ea, inset -21px 21px 42px #c98eff;
+  background: ${colors.primaryBrand};
+  box-shadow: inset 21px -21px 42px #a265e0, inset -21px 21px 42px #c07aff;
   overflow: hidden;
   margin-left: auto;
   margin-right: auto;
@@ -38,6 +38,6 @@ export const AvatarStyles = styled.div`
     height: 17.5vh;
     width: 17.5vh;
     margin: auto;
-    color: white;
+    color: #f2f2f2;
   }
 `;
