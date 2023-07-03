8b1e6ec68edaf1c23b8e2f72d0c4e9d32c4c22f3
d-dolin96 <d.dolinski1996@.com>
2023-07-03 14:56:59 -0400

    Routed

--- src/design/atoms/navbar/index.js
+++ src/design/atoms/navbar/index.js
@@ -5,23 +5,34 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { LogoNav } from "../logo/styles";
+import { Link } from "react-router-dom";
 
 function ToolBar({ onClick }) {
   return (
     <StyledBar>
-      <StyledTools>
-        <FontAwesomeIcon icon={faCalendar} />
-      </StyledTools>
+      <Link to="/Events">
+        <StyledTools>
+          <FontAwesomeIcon icon={faCalendar} />
+        </StyledTools>
+      </Link>
+
+      {/* I think we don't need this/we should move this feature to the events page? */}
       <StyledTools>
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </StyledTools>
       <LogoNav src="./images/aclogoangle.png" alt="AngelChoir Logo" />
-      <StyledTools>
-        <FontAwesomeIcon icon={faBell} />
-      </StyledTools>
-      <StyledTools>
-        <FontAwesomeIcon icon={faUser} />
-      </StyledTools>
+
+      <Link to="/Alerts">
+        <StyledTools>
+          <FontAwesomeIcon icon={faBell} />
+        </StyledTools>
+      </Link>
+
+      <Link to="/">
+        <StyledTools>
+          <FontAwesomeIcon icon={faUser} />
+        </StyledTools>
+      </Link>
     </StyledBar>
   );
 }
