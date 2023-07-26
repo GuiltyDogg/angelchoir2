8902c715189cfefd6f060ce9bb6266b4ab8d351c
GuiltyDogg <drewschultz1993@gmail.com>
2023-07-25 22:01:28 -0400

    new format event list

--- src/design/organisms/eventspage/index.js
+++ src/design/organisms/eventspage/index.js
@@ -8,6 +8,7 @@ function Events() {
       location: "The Masquerade",
       date: "June 15, 2023",
       time: "7PM - 10PM",
+      imageSrc: "./images/sampleflyer1.jpg",
     },
     // SHOW MULTIPLE EVENTS
     {
@@ -15,40 +16,38 @@ function Events() {
       title: "The Wiggles, and Friends",
       location: "Drunken Unicorn",
       date: "June 20, 2023",
+      imageSrc: "./images/sampleflyer1.jpg",
     },
-    {
-      id: 3,
-      title: "Taylor Swift",
-      location: "Not Sabbath Brewing",
-      date: "June 25, 2023",
-    },
-    {
-      id: 4,
-      title: "Blue Man Group",
-      location: "Aaron's Ampitheatre",
-      date: "June 29, 2023",
-    },
-    {
-      id: 5,
-      title: "Pathos",
-      location: "Swayze's",
-      date: "July 12, 2023",
-    },
+    // {
+    //   id: 3,
+    //   title: "Taylor Swift",
+    //   location: "Not Sabbath Brewing",
+    //   date: "June 25, 2023",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Blue Man Group",
+    //   location: "Aaron's Ampitheatre",
+    //   date: "June 29, 2023",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Pathos",
+    //   location: "Swayze's",
+    //   date: "July 12, 2023",
+    // },
   ];
 
   return (
     <EventDiv>
       <EventTitle>EVENTS</EventTitle>
       {events.map((event) => (
-        <EventBox>
-          <div key={event.id}>
-            <h2>{event.title}</h2>
-            <p>Location: {event.location}</p>
-            <p>Date: {event.date}</p>
-            <p>Time: {event.time}</p>
-          </div>
-          <EventImage src="./images/sampleflyer.jpg" alt="Your Image" />
-        </EventBox>
+        <EventImage
+          onClick={onClick}
+          key={event.id}
+          src="./images/sampleflyer.jpg"
+          alt="Your Image"
+        />
       ))}
     </EventDiv>
   );
