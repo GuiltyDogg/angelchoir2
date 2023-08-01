3ca4ffd3af1016e18d477f970c8da7156b99506d
GuiltyDogg <drewschultz1993@gmail.com>
2023-08-01 15:01:47 -0400

    closer

--- src/design/organisms/eventspage/styles.js
+++ src/design/organisms/eventspage/styles.js
@@ -1,56 +1,4 @@
-// import React, { useState } from "react";
-// import styled from "styled-components";
-// import colors from "../../subatomics/colors";
-// import whitespace from "../../subatomics/whitespace";
-// import fonts from "../../subatomics/fonts";
-// import borderradius from "../../subatomics/borderradius";
 
-// export const EventTitle = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   font-size: 50px;
-// `;
-
-// const BlurredImage = styled.img`
-//   max-width: 94vw;
-//   border-radius: ${borderradius.most};
-//   margin-bottom: ${whitespace.base};
-//   filter: ${(props) => (props.isBlurred ? "blur(4px)" : "none")};
-//   cursor: pointer;
-// `;
-
-// export const EventDiv = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-//   background-color: ${colors.lightGray};
-// `;
-
-// export const EventImage = ({ imageUrl = "/images/sampleflyer.jpg" }) => {
-//   const [isBlurred, setIsBlurred] = useState(false);
-
-//   const toggleBlur = () => {
-//     setIsBlurred(!isBlurred);
-//   };
-
-//   return (
-//     <div>
-//       <BlurredImage
-//         src={(imageUrl = "/images/sampleflyer.jpg")}
-//         alt="Event Image"
-//         isBlurred={isBlurred}
-//         onClick={toggleBlur}
-//       />
-//     </div>
-//   );
-// };
-
-
-
-
-
-// styles.js
 
 import React, { useState } from "react";
 import styled from "styled-components";
@@ -116,8 +64,10 @@ export const EventImage = ({ imageUrl = "/images/sampleflyer.jpg" }) => {
   };
 
   const toggleInfo = () => {
+    if (!showInfo) {
+      setIsBlurred(true); 
+    }
     setShowInfo((prevShowInfo) => !prevShowInfo);
-    setIsBlurred(true); // ISSUE HERE
   };
 
   return (
