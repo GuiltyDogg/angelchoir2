2f7c166ba25f742f2bdcf2684acc30ce6278d551
GuiltyDogg <drewschultz1993@gmail.com>
2023-08-08 21:56:17 -0400

    boo boo

--- src/design/organisms/eventscroll/index.js
+++ src/design/organisms/eventscroll/index.js
@@ -1,43 +1,10 @@
 import { EventBox, EventDiv, EventTitle } from "./styles";
 import EventImage from "../eventimage";
+import useEvents from "../../../state/hooks/useEvents";
 
 function Events({ onClick }) {
-  const events = [
-    {
-      id: 1,
-      title: "Sustenance, The Under the Moon Tour",
-      location: "The Masquerade",
-      date: "June 15, 2023",
-      time: "7PM - 10PM",
-      imageSrc: "./images/sampleflyer.jpg",
-    },
-    // SHOW MULTIPLE EVENTS
-    {
-      id: 2,
-      title: "The Wiggles, and Friends",
-      location: "Drunken Unicorn",
-      date: "June 20, 2023",
-      imageSrc: "/images/sampleflyer.jpg",
-    },
-    // {
-    //   id: 3,
-    //   title: "Taylor Swift",
-    //   location: "Not Sabbath Brewing",
-    //   date: "June 25, 2023",
-    // },
-    // {
-    //   id: 4,
-    //   title: "Blue Man Group",
-    //   location: "Aaron's Ampitheatre",
-    //   date: "June 29, 2023",
-    // },
-    // {
-    //   id: 5,
-    //   title: "Pathos",
-    //   location: "Swayze's",
-    //   date: "July 12, 2023",
-    // },
-  ];
+  const getEvents = useEvents();
+  const events = getEvents();
 
   return (
     <EventDiv>
