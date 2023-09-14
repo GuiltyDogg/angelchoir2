d2d4e0166b19968d3601ab1bca896d9158570ca8
ANTIM3CH <austinloganart@gmail.com>
2023-09-13 21:06:58 -0400

    Alert List: Stars now displayed from local storage

--- src/design/organisms/alertlist/index.js
+++ src/design/organisms/alertlist/index.js
@@ -1,4 +1,6 @@
 import { DateTime } from "luxon";
+import React, { useMemo, useState, useCallback, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Avatar from "../../atoms/avatar";
 import {
   EventBox,
@@ -7,8 +9,16 @@ import {
   EventTitle,
 } from "../eventscroll/styles";
 import StarBar from "../../atoms/starbar";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
+import { StarStyle } from "../../atoms/starbar/styles";
+import { StyledTools } from "../../atoms/navbar/styles";
 
 function AlertList() {
+  const defaultStarState = [false, false, false];
+
+  const [starState, setStarState] = useState(defaultStarState);
+
   const events = [
     {
       id: 1,
@@ -16,6 +26,7 @@ function AlertList() {
       username: "Not Drew",
       location: "The Masquerade",
       time: "2023-08-08T18:49:43.416Z",
+      starsSelected: starState.filter(Boolean).length,
     },
     {
       id: 2,
@@ -23,6 +34,7 @@ function AlertList() {
       username: "PimpCoatGuy",
       location: "The Masquerade",
       time: "2023-08-08T19:49:43.416Z",
+      starsSelected: starState.filter(Boolean).length,
     },
     {
       id: 3,
@@ -30,6 +42,7 @@ function AlertList() {
       username: "BVLLER",
       location: "The Masquerade",
       time: "2023-08-08T20:49:43.416Z",
+      starsSelected: starState.filter(Boolean).length,
     },
     {
       id: 4,
@@ -37,14 +50,55 @@ function AlertList() {
       username: "RunningOutOfUsernameIdeas",
       location: "The Masquerade",
       time: "2023-08-08T21:49:43.416Z",
+      starsSelected: starState.filter(Boolean).length,
     },
   ];
 
   const milliseconds = events.sort();
 
+  useEffect(() => {
+    const storedStarState = JSON.parse(localStorage.getItem("starState"));
+    if (storedStarState) {
+      setStarState(storedStarState);
+    }
+  }, []);
+
+  const sortedEvents = useMemo(() => {
+    return events.sort((a, b) => {
+      const timeA = DateTime.fromISO(a.time).toMillis();
+      const timeB = DateTime.fromISO(b.time).toMillis();
+      return timeA - timeB;
+    });
+  }, [events]);
+
+  const starIcons = (numStars) => {
+    const filledStars = Array.from({ length: numStars }).fill(faStar);
+    const emptyStars = Array.from({ length: 3 - numStars }).fill(faStarRegular);
+    const allStars = filledStars.concat(emptyStars);
+
+    return (
+      <div>
+        {filledStars.map((icon, index) => (
+          <span key={index}>
+            <FontAwesomeIcon
+              icon={icon}
+              style={{ color: "gold" }}
+              className="star-icon"
+            />
+          </span>
+        ))}
+        {emptyStars.map((icon, index) => (
+          <span>
+            <FontAwesomeIcon icon={icon} className="star-icon" />
+          </span>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <EventDiv>
-      {events.map((event) => {
+      {sortedEvents.map((event) => {
         const formattedDate = DateTime.fromISO(event.time).toLocaleString(
           DateTime.DATETIME_SHORT
         );
@@ -54,9 +108,9 @@ function AlertList() {
             <div>
               <Avatar disableUpload={true} />
             </div>
-            <div>
-              <StarBar />
-            </div>
+            <StarStyle>
+              <StyledTools>{starIcons(event.starsSelected)}</StyledTools>
+            </StarStyle>
             <p>User: {event.username}</p>
             <p>Time Alerted: {formattedDate}</p>
           </div>
